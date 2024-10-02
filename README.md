c775cca2248f07382d38f641bde6c0ca7cd74921
Jules KJ <juljen2730@gmail.com>
2024-10-02 22:06:23 +0200

    styled sheet

---
+++ README.md
@@ -0,0 +1,15 @@
+1. Lav nogle hardcodede filtreringer:
+
+   1. en der viser alle el drevne fartøjer
+
+   2. en der viser alle fartøjer med mere end 2 sæder
+
+   3. alle fartøjer el-drevne fartøjer ejet af Jonas
+
+   4. alle rugbrøds drevne fartøjer med plads til mere end en.
+
+2. Lav nogle if statements i showTheseVehicles funktionen så tabellen bliver smukkere: uden undefined og tomme felter og true;
+
+3. Flyt filtreringerne over på 4 knapper der viser de filtreringer der før var hardcodede + 1 knap til at vise alle (ufiltreret) (ikke sikker hvordan)
+
+4. Style tabellen endnu mere
---
+++ script.js
@@ -0,0 +1,100 @@
+const vehicles = [
+  {
+    type: "Bus",
+    fuel: "Diesel",
+    passengers: 45,
+    stops: ["Nørrebrogade", "Elmegade"],
+  },
+  { type: "Bil", fuel: "Benzin", passengers: 4, ownedBy: "Klaus" },
+  {
+    type: "Cykel",
+    fuel: "Rugbrød",
+    passengers: 0,
+    ownedBy: "Jonas",
+    isElectric: true,
+  },
+  { type: "Bil", passengers: 5, ownedBy: "Elon", isElectric: true },
+  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Fonda" },
+  {
+    type: "Cykel",
+    fuel: "Rugbrød",
+    passengers: 2,
+    ownedBy: "Vingegård",
+    isTandem: true,
+  },
+  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Yolanda" },
+  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Børge" },
+  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Jonas" },
+  { type: "Løbehjul", passengers: 1, isElectric: true },
+];
+const tbodyPointer = document.querySelector("tbody");
+showTheseVehicles(vehicles);
+
+function showTheseVehicles(arr) {
+  arr.forEach((each) => {
+    /*
+    let bgColor;
+    if (each.isElectric === true) {
+      bgColor = "lightgreen";
+    } else {
+      let bgColor = "";
+    }*/
+
+    let bgColor = each.isElectric ? "lightgreen" : "";
+
+    tbodyPointer.innerHTML += `<tr >
+  <td>${each.type || " "}</td>
+  <td>${each.fuel ? `${each.fuel}` : "El"}</td>
+  <td>${each.passengers || "none"}</td> 
+  <td>${each.stops ? `${each.stops}` : "No stops"}</td>
+  <td>${each.ownedBy || " "}</td>
+<td style="background-color: ${bgColor}; ">${
+      each.isElectric ? `Electric ${each.type}` : "Not Electric"
+    }</td>
+  <td>${each.isTandem || " "}</td>
+</tr>`;
+  });
+}
+
+const showElVehiclesBtn = document.querySelector(".showElVehicles");
+//her tjekker jeg om hvilke vehicles som har isEletric der er true
+
+showElVehiclesBtn.addEventListener("mousedown", isElVehicles);
+//her tjekker jeg om køretøjet er elektrik
+function isElVehicles(vehicles) {
+  console.log("clicked");
+  if (vehicles.isElectric === true) {
+    return vehicles;
+  }
+
+  const isEl = vehicles.filter(isElVehicles);
+  console.log(isEl);
+}
+
+//her tjekker jeg om passengers er større end 2
+function has2PlusSeats(vehicles) {
+  if (vehicles.passengers > 2) {
+    return vehicles.passengers;
+  }
+}
+
+const twoSeats = vehicles.filter(has2PlusSeats);
+console.log(twoSeats);
+//her tjekker jeg om de elertiske køretøjer er ejet af jonas
+function checkOwner(vehicles) {
+  if (vehicles.isElectric === true && vehicles.ownedBy === "Jonas") {
+    return vehicles.ownedBy;
+  }
+}
+
+const ownedByJonas = vehicles.filter(checkOwner);
+console.log(ownedByJonas);
+//her tjekker om de rugbrød fuled også kan have mere end en passesgere
+function rugbrod2PlusSeats(vehicles) {
+  if (vehicles.fuel === "Rugbrød" && vehicles.passengers > 1) {
+    return vehicles.passengers;
+  }
+}
+
+const rugbrodFueled = vehicles.filter(rugbrod2PlusSeats);
+console.log(rugbrodFueled);
